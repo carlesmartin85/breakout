59b217898417259272f76f4a2cf7b25c33cb756a
Tyler Dennis <ctdennis@protonmail.com>
2020-10-20 12:57:07 -0700

    Updated controller files

---
+++ build/CMakeFiles/Breakout.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "Breakout"
+  "Breakout.pdb"
+  "CMakeFiles/Breakout.dir/src/controller.cpp.o"
+  "CMakeFiles/Breakout.dir/src/game.cpp.o"
+  "CMakeFiles/Breakout.dir/src/main.cpp.o"
+  "CMakeFiles/Breakout.dir/src/renderer.cpp.o"
+  "CMakeFiles/Breakout.dir/src/snake.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Breakout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
